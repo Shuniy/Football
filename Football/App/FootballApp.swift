f0b27a62f17eca4680b10bb83c0d61da9a4e64c1
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-12-04 13:27:34 +0530

    Initial commit

---
+++ Football/App/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Shubham Kumar on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Football/Utils/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Football
+//
+//  Created by Shubham Kumar on 04/12/22.
+//
+
+import Foundation
